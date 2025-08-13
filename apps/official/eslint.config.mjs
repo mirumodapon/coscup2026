75021df881e3a2ef7073454f0ed506a5cef85071
Mirumo <mysteryperson520@gmail.com>
2025-08-13 19:54:42 +0800

    chore: add ESLint and configuration file with @anfu/eslint-config

---
+++ apps/official/eslint.config.mjs
@@ -0,0 +1,36 @@
+import antfu from '@antfu/eslint-config'
+
+//  ██████╗ ██████╗ ███████╗███╗   ██╗    ███████╗ ██████╗ ██╗   ██╗██████╗  ██████╗███████╗    ██╗███████╗     █████╗      ██████╗ ██╗███████╗████████╗
+// ██╔═══██╗██╔══██╗██╔════╝████╗  ██║    ██╔════╝██╔═══██╗██║   ██║██╔══██╗██╔════╝██╔════╝    ██║██╔════╝    ██╔══██╗    ██╔════╝ ██║██╔════╝╚══██╔══╝
+// ██║   ██║██████╔╝█████╗  ██╔██╗ ██║    ███████╗██║   ██║██║   ██║██████╔╝██║     █████╗      ██║███████╗    ███████║    ██║  ███╗██║█████╗     ██║
+// ██║   ██║██╔═══╝ ██╔══╝  ██║╚██╗██║    ╚════██║██║   ██║██║   ██║██╔══██╗██║     ██╔══╝      ██║╚════██║    ██╔══██║    ██║   ██║██║██╔══╝     ██║
+// ╚██████╔╝██║     ███████╗██║ ╚████║    ███████║╚██████╔╝╚██████╔╝██║  ██║╚██████╗███████╗    ██║███████║    ██║  ██║    ╚██████╔╝██║██║        ██║
+//  ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═══╝    ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚══════╝    ╚═╝╚══════╝    ╚═╝  ╚═╝     ╚═════╝ ╚═╝╚═╝        ╚═╝
+
+export default antfu({
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
+
+  typescript: true,
+  jsonc: true,
+  yaml: true,
+  vue: true,
+}, {
+  rules: {
+    'curly': ['error', 'multi-line', 'consistent'],
+    'node/prefer-global/buffer': ['error', 'always'],
+    'node/prefer-global/process': ['error', 'always'],
+    'style/arrow-parens': ['error', 'always'],
+    'style/brace-style': ['error', 'stroustrup'],
+    'style/operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
+    'style/max-len': ['error', { code: 120, ignoreComments: true, ignoreUrls: true }],
+    'vue/max-attributes-per-line': ['error'],
+    'vue/attributes-order': ['error', { alphabetical: true }],
+  },
+}, {
+  rules: {
+    'no-console': ['warn'],
+  },
+})
