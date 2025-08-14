39f511f129df8d32d586405e533af6aec726911b
Mirumo <mysteryperson520@gmail.com>
2025-08-14 21:33:34 +0800

    chore: install and configure Tailwind CSS
    - Installed Tailwind CSS and its vite plugin via pnpm and initialized
      tailwind vite plugin
    - create global css file and import tailwindcss

--- apps/official/vite.config.ts
+++ apps/official/vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
