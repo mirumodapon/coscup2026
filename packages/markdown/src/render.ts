73dec88714c76b4761f37e70dbdabbe8909982b8
Mirumo <mysteryperson520@gmail.com>
2025-08-16 00:07:02 +0800

    feat(markdown): add markdown-it plugin

--- packages/markdown/src/render.ts
+++ packages/markdown/src/render.ts
@@ -1,8 +1,33 @@
 import MarkdownIt from 'markdown-it'
+import markdownItAbbr from 'markdown-it-abbr'
+import markdownItAnchor from 'markdown-it-anchor'
+import markdownitContainer from 'markdown-it-container'
+import markdownItDeflist from 'markdown-it-deflist'
+import { full as markdownItEmoji } from 'markdown-it-emoji'
+import markdownItFootnote from 'markdown-it-footnote'
+import markdownItIns from 'markdown-it-ins'
+import markdownItMark from 'markdown-it-mark'
+import markdownItSub from 'markdown-it-sub'
+import markdownItSup from 'markdown-it-sup'
 
 const md = MarkdownIt({
   html: true,
   linkify: true,
 })
+
+md.use(markdownItAbbr)
+md.use(markdownItAnchor)
+md.use(markdownItDeflist)
+md.use(markdownItEmoji)
+md.use(markdownItFootnote)
+md.use(markdownItIns)
+md.use(markdownItMark)
+md.use(markdownItSub)
+md.use(markdownItSup)
+
+md.use(markdownitContainer, 'error')
+md.use(markdownitContainer, 'info')
+md.use(markdownitContainer, 'success')
+md.use(markdownitContainer, 'warning')
 
 export default md
--- packages/markdown/src/types/index.d.ts
+++ packages/markdown/src/types/index.d.ts
@@ -4,3 +4,15 @@ declare module '*.vue' {
   const component: DefineComponent<object, object, any>
   export default component
 }
+
+declare module 'markdown-it'
+declare module 'markdown-it-abbr'
+declare module 'markdown-it-anchor'
+declare module 'markdown-it-container'
+declare module 'markdown-it-deflist'
+declare module 'markdown-it-emoji'
+declare module 'markdown-it-footnote'
+declare module 'markdown-it-ins'
+declare module 'markdown-it-mark'
+declare module 'markdown-it-sub'
+declare module 'markdown-it-sup'
