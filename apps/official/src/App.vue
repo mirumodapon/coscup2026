afe70a01081d3a685a4fe3aaf65dd22a7fe9717f
Mirumo <mysteryperson520@gmail.com>
2025-08-16 00:51:40 +0800

    refactor: move theme import from `main.ts` to `App.vue`

--- apps/official/src/App.vue
+++ apps/official/src/App.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import 'themes/official'
+</script>
+
 <template>
   <h1>Hello world!</h1>
 </template>
--- apps/official/src/main.ts
+++ apps/official/src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
-import 'themes/official'
 
 createApp(App).mount('#app')
