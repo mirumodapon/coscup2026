997c36acc9ab8b8435d4e466e64c22ca12d218eb
Mirumo <mysteryperson520@gmail.com>
2025-08-16 14:23:19 +0800

    feat(icons): add package

--- apps/official/vite.config.ts
+++ apps/official/vite.config.ts
@@ -1,12 +1,19 @@
 import { resolve } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import { ExternalPackageIconLoader } from 'unplugin-icons/loaders'
 import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), Icons()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Icons({
+      customCollections: ExternalPackageIconLoader('icons'),
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
---
+++ packages/icons/index.js
@@ -0,0 +1,9 @@
+const chars = require('./chars.json');
+const icons = require('./icons.json');
+const info = require('./info.json');
+const metadata = require('./metadata.json');
+
+exports.icons = icons;
+exports.info = info;
+exports.metadata = metadata;
+exports.chars = chars;
