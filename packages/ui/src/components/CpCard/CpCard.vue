1ea7549cdebc4d512f2e37e584c5a66cc0097903
Mirumo <mysteryperson520@gmail.com>
2025-09-07 22:11:32 +0800

    feat(ui): add CpCard

---
+++ packages/ui/src/components/CpCard/CpCard.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+defineProps<{ title: string }>()
+</script>
+
+<template>
+  <div class="cp-card">
+    <div class="cp-card__content-wrap">
+      <h2 class="cp-card__title">
+        {{ title }}
+      </h2>
+      <div class="cp-card__content">
+        <slot name="content" />
+      </div>
+      <div class="cp-card__actions">
+        <slot name="actions" />
+      </div>
+    </div>
+    <div class="cp-card__image-wrap">
+      <slot name="image" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cp-card {
+  @apply w-[600px] h-[240px] bg-white flex flex-row rounded-2xl overflow-hidden;
+  @apply shadow-md;
+}
+
+.cp-card__content-wrap {
+  @apply p-4 flex flex-col flex-1 text-gray-700;
+}
+
+.cp-card__title {
+  @apply text-2xl font-bold;
+}
+
+.cp-card__content {
+  @apply mt-2 flex-1;
+}
+
+.cp-card__image-wrap {
+  @apply basis-[230px] overflow-hidden;
+}
+
+.cp-card__image-wrap > * {
+  @apply w-full h-full object-cover;
+}
+</style>
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as CpButton } from './CpButton/CpButton.vue'
+export { default as CpCard } from './CpCard/CpCard.vue'
