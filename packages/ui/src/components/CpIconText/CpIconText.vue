8537c31a33028aaa955bb4ca7276a837438e5ab6
Mirumo <mysteryperson520@gmail.com>
2025-09-13 01:13:41 +0800

    feat(ui): add icon text

---
+++ packages/ui/src/components/CpIconText/CpIconText.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    is?: string
+    text: string
+    positionX?: 'left' | 'right' | 'center'
+    positionY?: 'top' | 'bottom' | 'middle'
+    icon: Component
+  }>(),
+  {
+    is: 'span',
+    positionX: 'left',
+    positionY: 'middle',
+  },
+)
+</script>
+
+<template>
+  <div
+    class="cp-icon-text"
+    :class="[`cp-icon-text--${positionX}`, `cp-icon-text--${positionY}`]"
+  >
+    <component :is="is">
+      {{ text }}
+    </component>
+    <component
+      :is="icon"
+      class="cp-icon-text__icon"
+    />
+  </div>
+</template>
+
+<style>
+.cp-icon-text {
+  @apply grid grid-template-rows-[1fr_1fr_1fr] grid-template-columns-[1fr_1fr_1fr];
+}
+
+.cp-icon-text > *:first-child {
+  @apply col-start-2 col-end-3 row-start-2 row-end-3;
+}
+
+.cp-icon-text--left > .cp-icon-text__icon {
+  @apply col-start-1 col-end-2;
+}
+
+.cp-icon-text--right > .cp-icon-text__icon {
+  @apply col-start-3 col-end-4;
+}
+
+.cp-icon-text--center > .cp-icon-text__icon {
+  @apply col-start-2 col-end-3;
+}
+
+.cp-icon-text--top > .cp-icon-text__icon {
+  @apply row-start-1 row-end-2;
+}
+
+.cp-icon-text--bottom > .cp-icon-text__icon {
+  @apply row-start-3 row-end-4;
+}
+
+.cp-icon-text--middle > .cp-icon-text__icon {
+  @apply row-start-2 row-end-3;
+}
+</style>
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as CpButton } from './CpButton/CpButton.vue'
 export { default as CpCard } from './CpCard/CpCard.vue'
+export { default as CpIconText } from './CpIconText/CpIconText.vue'
 export { default as CpSimpleSponsorCard } from './CpSimpleSponsorCard/CpSimpleSponsorCard.vue'
