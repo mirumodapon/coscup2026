041a0b67ecf9ba42ee45b19f62e25d5e2b749402
Mirumo <mysteryperson520@gmail.com>
2025-09-14 21:41:32 +0800

    fix(official): i18n change with router path

--- apps/official/src/App.vue
+++ apps/official/src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
+import { watch } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
 
 const { locale } = useI18n()
 const route = useRoute()
+const router = useRouter()
 
-locale.value = route.params.lang
+watch(
+  router.currentRoute,
+  () => {
+    locale.value = route.params.lang
+  },
+  { immediate: true },
+)
 </script>
 
 <template>
