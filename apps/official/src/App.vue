f7eefb88288a58f5c3810dabf7c3286b75ed73ef
Mirumo <mysteryperson520@gmail.com>
2025-09-14 15:17:19 +0800

    feat(official): add navbar component

--- apps/official/src/App.vue
+++ apps/official/src/App.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+</script>
+
 <template>
+  <Navbar />
   <main>
     <router-view />
   </main>
---
+++ apps/official/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import ExternalLinkIcon from '~icons/tabler/external-link'
+import LanguageIcon from '~icons/tabler/language'
+import logoImg from '@/assets/img/coscup-logo.png'
+
+const menu = [
+  { name: '議程表', path: '#' },
+  { name: '議程狀態', path: '#' },
+  { name: '交通方式', path: '#' },
+  { name: '會場地圖', path: '#' },
+  { name: '贊助夥伴', path: '#' },
+  { name: '周邊活動', path: '#' },
+  { name: '工作人員', path: '#' },
+  { name: '部落格', path: '#', isExternal: true },
+  { name: '社群守則', path: '#', isExternal: true },
+]
+</script>
+
+<template>
+  <nav class="flex justify-center items-center bg-white shadow-lg h-16 text-gray-700">
+    <div class="basis-[180px] h-full shrink-0 px-3 py-1">
+      <img
+        class="w-inherit h-inherit"
+        :src="logoImg"
+      >
+    </div>
+    <div class="h-full grow-1 overflow-auto">
+      <ul class="h-inherit flex justify-center items-center gap-3">
+        <li
+          v-for="item in menu"
+          :key="item.name"
+          class="whitespace-nowrap"
+        >
+          <component
+            :is="item.isExternal ? 'a' : 'router-link'"
+            :href="item.path"
+            :to="item.path"
+          >
+            {{ item.name }}
+            <ExternalLinkIcon
+              v-if="item.isExternal"
+              class="inline"
+            />
+          </component>
+        </li>
+      </ul>
+    </div>
+    <div class="text-[24px] flex justify-end items-center basis-[180px] px-3 py-1">
+      <button class="cursor-pointer transition hover:scale-105 active:scale-95">
+        <LanguageIcon />
+      </button>
+    </div>
+  </nav>
+</template>
