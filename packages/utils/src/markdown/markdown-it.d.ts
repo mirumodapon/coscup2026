b5d0141ebc22343f07c9d5b2ae5af0cd0e17c8b6
miru <mail@mirumo.org>
2025-09-19 09:48:40 +0800

    fix(utils/markdown): markdown it plugin type

---
+++ packages/utils/src/markdown/markdown-it.d.ts
@@ -0,0 +1,69 @@
+declare module 'markdown-it-container' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  const plugin: SimplePlugin
+  export default plugin
+}
+
+declare module 'markdown-it-abbr' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  const plugin: SimplePlugin
+  export default plugin
+}
+
+declare module 'markdown-it-deflist' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  const plugin: SimplePlugin
+  export default plugin
+}
+
+declare module 'markdown-it-emoji' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  const plugin: SimplePlugin
+  export default plugin
+}
+
+declare module 'markdown-it-footnote' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  const plugin: SimplePlugin
+  export default plugin
+}
+
+declare module 'markdown-it-ins' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  const plugin: SimplePlugin
+  export default plugin
+}
+
+declare module 'markdown-it-mark' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  const plugin: SimplePlugin
+  export default plugin
+}
+
+declare module 'markdown-it-sub' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  const plugin: SimplePlugin
+  export default plugin
+}
+
+declare module 'markdown-it-sup' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  const plugin: SimplePlugin
+  export default plugin
+}
+
+declare module 'markdown-it-emoji' {
+  import type { SimplePlugin } from 'markdown-it'
+
+  export const full: SimplePlugin
+}
+
