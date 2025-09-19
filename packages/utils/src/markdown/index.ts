7013d16a38d9624bd20d84e317a263c5cc94366f
miru <mail@mirumo.org>
2025-09-19 09:52:36 +0800

    refactor(utils/markdown): restructure Markdown implementation by extending MarkdownIt

--- packages/utils/src/markdown/index.ts
+++ packages/utils/src/markdown/index.ts
@@ -1,8 +1,5 @@
-import DOMPurify from 'dompurify'
-import MarkdownIt from 'markdown-it'
-import markdownItAnchor from 'markdown-it-anchor'
-import markdownitContainer from 'markdown-it-container'
 import markdownItAbbr from 'markdown-it-abbr'
+import markdownItAnchor from 'markdown-it-anchor'
 import markdownItDeflist from 'markdown-it-deflist'
 import { full as markdownItEmoji } from 'markdown-it-emoji'
 import markdownItFootnote from 'markdown-it-footnote'
@@ -10,36 +7,25 @@ import markdownItIns from 'markdown-it-ins'
 import markdownItMark from 'markdown-it-mark'
 import markdownItSub from 'markdown-it-sub'
 import markdownItSup from 'markdown-it-sup'
+import { Markdown } from './markdown'
+import markdownContainer from './plugins/container'
 
-const md = new MarkdownIt({
+const md = new Markdown({
   html: true,
   linkify: true,
   typographer: true,
+  plugins: [
+    markdownItAbbr,
+    markdownItAnchor,
+    markdownItDeflist,
+    markdownItEmoji,
+    markdownItFootnote,
+    markdownItIns,
+    markdownItMark,
+    markdownItSub,
+    markdownItSup,
+    markdownContainer,
+  ],
 })
-
-md.use(markdownItAbbr)
-md.use(markdownItAnchor)
-md.use(markdownItDeflist)
-md.use(markdownItEmoji)
-md.use(markdownItFootnote)
-md.use(markdownItIns)
-md.use(markdownItMark)
-md.use(markdownItSub)
-md.use(markdownItSup)
-
-md.use(markdownitContainer, 'container', {
-  validate(params: string) {
-    return ['error', 'info', 'success', 'warning'].includes(params.trim())
-  },
-  render(tokens: any, idx: number) {
-    if (tokens[idx].nesting === 1) {
-      const type = tokens[idx].info.trim()
-      return `<div class="container container-${type}">`
-    }
-    return '</div>\n'
-  },
-})
-
-md.safeRender = (str: string) => DOMPurify.sanitize(md.render(str))
 
 export default md
