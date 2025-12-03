c9d86a406502d1e63c6bc8ab99af49e239371d76
Riley Ho <riley@rileychh.dev>
2025-12-03 22:55:46 +0800

    fix: add TypeScript config for unplugin-vue-router typed routes

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <script setup lang="ts">
-import { watch } from 'vue'
-import { useI18n } from 'vue-i18n'
-import { useRoute, useRouter } from 'vue-router'
 import IconCpCoscupSolid from '~icons/cp/coscup-solid'
 import Navbar from '@/components/Navbar.vue'
-
-const { locale } = useI18n()
-const route = useRoute()
-const router = useRouter()
-
-watch(
-  router.currentRoute,
-  () => {
-    locale.value = route.params.lang
-  },
-  { immediate: true },
-)
 </script>
 
 <template>
